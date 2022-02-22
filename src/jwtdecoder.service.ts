42525a0df8267f48ef36c834c8ffc8b6e7844e17
aridaisantana <aridaitecnologia@gmail.com>
2022-02-22 14:45:38 +0000

    IMPROVEMENT: decoder service signature verification improved.

--- src/jwtdecoder.service.ts
+++ src/jwtdecoder.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { createHmac, sign } from 'crypto';
+import { createHmac } from 'crypto';
 
 @Injectable()
 export class JwtDecoderService {
@@ -8,8 +8,15 @@ export class JwtDecoderService {
     if (jwtParts.length === 3) {
       const header = Buffer.from(jwtParts[0], 'base64url').toString('utf-8');
       const payload = Buffer.from(jwtParts[1], 'base64url').toString('utf-8');
-      const signature = Buffer.from(jwtParts[2], 'base64url').toString('utf-8');
-      console.log(header, payload, signature);
+      const hash = createHmac('SHA256', secret)
+        .update(jwtParts[0] + '.' + jwtParts[1])
+        .digest('hex');
+      const signatureHash = jwtParts[2];
+      console.log('Header:' + header);
+      console.log('Payload:' + payload);
+      hash === signatureHash
+        ? console.log('Verified:Success')
+        : console.log('Verified:Failure');
     }
   }
 }
